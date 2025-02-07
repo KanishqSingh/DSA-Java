27f751cced47ec6aa6373046480ae3db3b05a476
Kanishq SIngh <singhkanishq06@gmail.com>
2025-02-07 14:16:40 +0530

    Fourth commit

---
+++ Linkedlist/LoopLL.java
@@ -0,0 +1,54 @@
+package Linkedlist;
+
+
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LoopLL {
+    Node head;
+
+    void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    boolean hasLoop() {
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LoopLL list = new LoopLL();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        list.head.next.next.next.next.next = list.head.next; 
+
+        System.out.println("Loop detected: " + list.hasLoop());
+    }
+
+}
