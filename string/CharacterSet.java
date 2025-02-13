0fa38222f2a82c72eb903b92e0e2982f441dafe0
Kanishq SIngh <singhkanishq06@gmail.com>
2025-02-13 21:22:17 +0530

    First

---
+++ string/CharacterSet.java
@@ -0,0 +1,22 @@
+package string;
+
+
+public class CharacterSet {
+    public static void main(String[] args) {
+        char[] set = {'a', 'b', 'c'};
+        int k = 2;
+        System.out.println("All possible strings of length " + k + ":");
+        generateStrings(set, "", k);
+    }
+
+    public static void generateStrings(char[] set, String prefix, int k) {
+        if (k == 0) {
+            System.out.println(prefix);
+            return;
+        }
+
+        for (char c : set) {
+            generateStrings(set, prefix + c, k - 1);
+        }
+    }
+}
