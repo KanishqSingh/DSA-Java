1775e65d09d5a6bce9a670c8351bd16596d416e9
Kanishq SIngh <singhkanishq06@gmail.com>
2025-03-09 22:08:37 +0530

    Initial commit

---
+++ Queue/ReverseQueue.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class ReverseQueue {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+
+        reverseQueue(queue);
+
+        while (!queue.isEmpty()) {
+            System.out.print(queue.poll() + " ");
+        }
+    }
+
+    public static void reverseQueue(Queue<Integer> queue) {
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()) {
+            stack.push(queue.poll());
+        }
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+    }
+}
