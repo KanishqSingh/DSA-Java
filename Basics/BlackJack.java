d5a198f2d47d5861f7f675b2b5bbed7b2f8f3295
Kanishq SIngh <singhkanishq06@gmail.com>
2025-03-14 18:13:54 +0530

    Initial

---
+++ Basics/BlackJack.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class Blackjack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        while (T-- > 0) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            
+            int required = 21 - (A + B);
+            
+            if (required >= 1 && required <= 10) {
+                System.out.println(required);
+            } else {
+                System.out.println(-1);
+            }
+        }
+        sc.close();
+    }
+}
