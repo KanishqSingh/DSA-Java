2e1302d444314d93fe2d4993d3028053b09141d9
Kanishq SIngh <singhkanishq06@gmail.com>
2025-04-02 22:28:42 +0530

    initial

--- recursion/Josephus.java
+++ recursion/Josephus.java
@@ -11,8 +11,20 @@ public class Josephus {
         } 
         return result;
     }
+    
+  //recursion
+    static int jos1(int n , int k){
+
+        if(n==1){
+            return 0;
+        }
+        
+  
+        return (jos1(n - 1, k) + k) % n;
+    }
 	public static void main(String[] args) {
 		System.out.println(jos(2,3)+1);
+        System.out.println(jos1(2,3)+1);
 	}
 
 }
