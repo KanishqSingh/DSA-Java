ad17818104033daae05a030991ce92cbdc22507b
Kanishq SIngh <singhkanishq06@gmail.com>
2025-04-22 21:03:19 +0530

    initial commit

---
+++ recursion/TowerOfHanoi.java
@@ -0,0 +1,25 @@
+public class TowerOfHanoi {
+
+    
+    static void solveHanoi(int n, char source, char auxiliary, char destination) {
+   
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
+            return;
+        }
+
+        solveHanoi(n - 1, source, destination, auxiliary);
+
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+
+
+        solveHanoi(n - 1, auxiliary, source, destination);
+    }
+
+    public static void main(String[] args) {
+        int numDisks = 3; 
+
+        System.out.println("Solution for Tower of Hanoi with " + numDisks + " disks:\n");
+        solveHanoi(numDisks, 'A', 'B', 'C');  
+    }
+}
