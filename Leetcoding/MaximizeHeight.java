bc3c58b4e07d0de795402182bb8676b6f6652c0f
Kanishq SIngh <singhkanishq06@gmail.com>
2025-04-29 22:54:11 +0530

    initial commit

---
+++ Leetcoding/MaximizeHeight.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class MaximizeHeight {
+    public int maximumSum(List<Integer> maximumHeights) {
+        Collections.sort(maximumHeights, Collections.reverseOrder());
+        int n = maximumHeights.size();
+        int prevHeight = Integer.MAX_VALUE;
+        long total = 0;
+
+        for (int h : maximumHeights) {
+            int heightToAssign = Math.min(h, prevHeight - 1);
+            if (heightToAssign <= 0) return -1;
+            total += heightToAssign;
+            prevHeight = heightToAssign;
+        }
+
+        return (int) total;
+    }
+}
