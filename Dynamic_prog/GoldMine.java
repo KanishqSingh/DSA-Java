5963b3c6427ac8f5d3b0c8f1e3e318265d64951e
Kanishq SIngh <singhkanishq06@gmail.com>
2025-07-13 23:31:37 +0530

    initial commit

---
+++ Dynamic_prog/GoldMine.java
@@ -0,0 +1,60 @@
+
+package Dynamic_Prog;
+
+import java.util.*;
+
+public class GoldMine {
+
+    public static void main(String[] args) {
+
+        int[][] arr = {
+                { 1, 3, 1, 5 },
+                { 2, 2, 4, 1 },
+                { 5, 0, 2, 3 },
+                { 0, 6, 1, 2 }
+        };
+
+        int n = arr.length;
+        int m = arr[0].length;
+
+        int[][] dp = new int[n][m];
+        for (int j = m - 1; j >= 0; j--) {
+            for (int i = n - 1; i >= 0; i--) {
+
+                if (j == arr[0].length - 1) {
+
+                    dp[i][j] = arr[i][j];
+
+                } else if (i == 0) {
+
+                    dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i + 1][j + 1]);
+
+                } else if (i == arr.length - 1) {
+
+                    dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i - 1][j + 1]);
+
+                } else {
+
+                    dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], Math.max(dp[i - 1][j + 1], dp[i + 1][j + 1]));
+
+                }
+
+            }
+
+        }
+
+        int max = dp[0][0];
+        for (int i = 0; i < dp.length; i++) {
+
+            if (dp[i][0] > max) {
+                max = dp[i][0];
+
+            }
+
+        }
+
+        System.out.println(max);
+
+    }
+
+}
