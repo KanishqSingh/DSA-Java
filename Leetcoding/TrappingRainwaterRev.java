7e7d04b425742875e72ab1a2e4cd76aa4f982459
Kanishq SIngh <singhkanishq06@gmail.com>
2025-08-14 23:26:28 +0530

    revision of trapping rainwater

---
+++ Leetcoding/TrappingRainwaterRev.java
@@ -0,0 +1,27 @@
+package Leetcoding;
+
+public class TrappingRainwaterRev {
+
+    public static int calculateheight(int[] height){
+        int l=0,r=height.length -1,lmax=0,rmax=0,ans=0;
+        while(l<r){
+            lmax = Math.max(lmax,height[l]);
+            rmax = Math.max(rmax,height[r]);
+            if(lmax < rmax){
+                ans+=lmax - height[l];
+                l++;
+            }else{
+                ans+=rmax- height[r];
+                r--;
+            }
+            
+        }return ans;
+    }
+
+        public static void main(String[] args) {
+        
+        int[] height = {0,1,0,2,4,0,1,3,6,0,0,1,4};
+        System.out.println(calculateheight(height));
+    }
+    
+}
