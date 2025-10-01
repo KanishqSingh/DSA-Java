726045af852f6e000d2442af651652f478e8042b
Kanishq SIngh <singhkanishq06@gmail.com>
2025-10-01 22:11:02 +0530

    initial commit

---
+++ Leetcoding/SumOfSubstring.java
@@ -0,0 +1,22 @@
+public class SumOfSubstring {
+    
+    public static int maxSum(int[] arr, int k) {
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i <= arr.length - k; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += arr[j];
+            }
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+        System.out.println(maxSum(arr, k)); 
+    }
+}
+
+
