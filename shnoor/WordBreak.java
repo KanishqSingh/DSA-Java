8603cae3399e662760cc3b943c09a4ca3bd18e3d
Kanishq SIngh <singhkanishq06@gmail.com>
2025-11-12 23:19:39 +0530

    initial

---
+++ shnoor/WordBreak.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class WordBreak {
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true; // Empty string can always be segmented
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        List<String> wordDict = Arrays.asList("leet", "code");
+        
+        System.out.println(wordBreak(s, wordDict)); // Output: true
+    }
+}
