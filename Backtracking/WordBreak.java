4dd245e57d1965892a5a5f507d3987d0b0539e1b
Kanishq SIngh <singhkanishq06@gmail.com>
2025-11-23 23:29:32 +0530

    initial commit

--- Backtracking/WordBreak.java
+++ Backtracking/WordBreak.java
@@ -1,9 +1,11 @@
+import java.util.List;
+
 public class WordBreak {
    
     public boolean wordBreak(String s, List<String> wordDict) {
 
         boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;   // empty string is always valid
+        dp[0] = true;   
 
         for (int i = 1; i <= s.length(); i++) {
 
