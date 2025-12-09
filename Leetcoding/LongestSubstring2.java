3bf676799ea072efefecd75df8b83f45e3e0fa7b
Kanishq SIngh <singhkanishq06@gmail.com>
2025-12-09 23:32:30 +0530

    new commit

---
+++ Leetcoding/LongestSubstring2.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+public class LongestSubstring2 {
+    
+
+    public static int longestSubstring(String s, int k) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            map.put(rightChar, map.getOrDefault(rightChar, 0) + 1);
+
+            while (map.size() > k) {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                if (map.get(leftChar) == 0) {
+                    map.remove(leftChar);
+                }
+                left++; 
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        String s = "aabacbebebe";
+        int k = 3;
+        System.out.println(longestSubstring(s, k)); 
+    }
+
+
+}
